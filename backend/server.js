78d126c7f119d4dd1383aa5e3df4159003b87ef3
niharika2k00 <dniharika16@gmail.com>
2021-09-22 15:25:38 +0530

    sdf

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,7 @@ app.use(express.static(path.join(__dirname, './uploads')))
 
 
 // FOR DEPLOYMENT  ______________-  LAST STEP  ______________
-if (process.env.NODE_ENV === "production purpose") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '/frontend/build')))  // make build as a static folder
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
 }
